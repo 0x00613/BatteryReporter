5e0e60f28d072a2d6b4bdd05cbfebd550c3ea14b
David Kim <xeros.log@gmail.com>
2024-07-05 18:02:44 +0900

    Fixed: 라이브러리 참조 경로 에러 수정
    - 라이브러리 참조명 오타로 인한 버그 수정
    
    - 기존 배포 빌드가 외부 라이브러리를 불러오지 못해 실행이 되지 않던 문제 수정하여 재배포

--- BatteryReporter.py
+++ BatteryReporter.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 from bs4 import BeautifulSoup
 import webbrowser
 import subprocess
-from lib import calcModule
+from libs import calcModule
 
 softwareVersion = '1.0.0beta'
 softwareName = 'Battery Reporter'
